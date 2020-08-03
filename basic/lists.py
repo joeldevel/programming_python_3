3e0c65e5c686a4660cbbad0e0cdc357281b8db67
joeldevel <christianjoel.developer@gmail.com>
2020-08-02 21:31:24 -0300

    Added some methods

--- basic/lists.py
+++ basic/lists.py
@@ -5,8 +5,29 @@
     date: Augus 2020
     usage: in the console: python3 lists.py
 '''
+# A helper to make more readable the output
+def separator( description='' ):
+    print( 30 * '_' )
+    print( description.upper() )
+
+def showResults( list, description ):
+    separator( description )
+    for element in list:
+        print( f"We have a {element}")
+
+
 # A list is a sequence of elements
 ourList = [ 'pencil', 'sketchbook', 'sharpener']
 
-for element in ourList:
-    print( f"We have a {element}")
+showResults( ourList, 'a list')
+
+# We can add an element to the end
+ourList.append('brush');
+showResults( ourList, 'append')
+
+# or in some position
+ourList.insert(2, "calculator")
+showResults( ourList, 'insert')
+
+ourList.pop(-1);
+showResults( ourList, 'pop')
