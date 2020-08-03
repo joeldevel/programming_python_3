32a74283a29d0b00a88a50a5ad81b601628eaef8
joeldevel <christianjoel.developer@gmail.com>
2020-08-02 21:10:35 -0300

    Starting lists

---
+++ basic/lists.py
@@ -0,0 +1,12 @@
+'''
+    name: lists.py
+    purpose: show basic python 3 lists
+    author: joeldevel
+    date: Augus 2020
+    usage: in the console: python3 lists.py
+'''
+# A list is a sequence of elements
+ourList = [ 'pencil', 'sketchbook', 'sharpener']
+
+for element in ourList:
+    print( f"We have a {element}")
